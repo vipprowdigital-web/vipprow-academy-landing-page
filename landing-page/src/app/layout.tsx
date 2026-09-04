745f99d2b4d432df060b1e3d463c58bee6ff47d2
ArpitaHalvi <arpitahalvi@gmail.com>
2026-09-04 18:47:12 +0530

    Added GTM

--- landing-page/src/app/layout.tsx
+++ landing-page/src/app/layout.tsx
@@ -42,6 +42,16 @@ export default async function RootLayout({
 
   return (
     <html lang="en" className={`${bricolage.variable} dark h-full antialiased`}>
+      <head>
+        {/* Google Tag Manager */}
+        <Script id="gtm-script" strategy="afterInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-5KKSMN7L');`}
+        </Script>
+      </head>
       <body className="min-h-full flex flex-col bg-background">
         {/* Google Tag Manager (noscript) */}
         <noscript>
@@ -54,13 +64,13 @@ export default async function RootLayout({
         </noscript>
 
         {/* Google Tag Manager */}
-        <Script id="gtm" strategy="afterInteractive">
+        {/* <Script id="gtm" strategy="afterInteractive">
           {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
           new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
           j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
           })(window,document,'script','dataLayer','GTM-5KKSMN7L');`}
-        </Script>
+        </Script> */}
 
         <PageLoadShutter />
 
